ffb8db5f6ebce6fed876ed9110716bfde1351263
Coder 79 <dev.ahmed.saeed@gmail.com>
2018-01-28 21:47:54 +0200

    Api Connect

--- api.php
+++ api.php
@@ -6,7 +6,7 @@ $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 $input = json_decode(file_get_contents('php://input'),true);
 
 // connect to the mysql database
-$link = mysqli_connect('localhost', 'root', '', 'form');
+$link = mysqli_connect('sql2.freemysqlhosting.net', 'sql2218325', 'uR9*jA7*', 'sql2218325');
 mysqli_set_charset($link,'utf8');
 
 // retrieve the table and key from the path
