9a00634d04f61ce1c9a9aef9b8ee01cd3f33aef9
Coder 79 <dev.ahmed.saeed@gmail.com>
2018-01-28 21:30:41 +0200

    My DB

--- db.php
+++ db.php
@@ -1,33 +1,11 @@
 <?php
 ob_start();
 session_start();
-define('DBHOST','localhost');
-define('DBUSER','root');
-define('DBPASS','');
-define('DBNAME','form');
+define('DBHOST','sql2.freemysqlhosting.net');
+define('DBUSER','sql2218325');
+define('DBPASS','uR9*jA7*');
+define('DBNAME','sql2218325');
 $db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-date_default_timezone_set('Europe/London');
-$ip = $_SERVER['REMOTE_ADDR'];
-$ip_host = $_SERVER['HTTP_HOST'];
-$proxy_headers = array(
-	'HTTP_VIA',
-	'HTTP_X_FORWARDED_FOR',
-	'HTTP_FORWARDED_FOR',
-	'HTTP_X_FORWARDED',
-	'HTTP_FORWARDED',
-	'HTTP_CLIENT_IP',
-	'HTTP_FORWARDED_FOR_IP',
-	'VIA',
-	'X_FORWARDED_FOR',
-	'FORWARDED_FOR',
-	'X_FORWARDED',
-	'FORWARDED',
-	'CLIENT_IP',
-	'FORWARDED_FOR_IP',
-	'HTTP_PROXY_CONNECTION'
-);
-foreach($proxy_headers as $x){
-	if (isset($_SERVER[$x])) die("You are using a proxy!");
-}
+//date_default_timezone_set('Europe/London');
 ?>
