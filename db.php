a99f6b0b9f23f0e1f9f149180207051af45cc980
Coder 79 <yahongie2014@users.noreply.github.com>
2018-02-15 17:32:25 +0200

    Update db.php

--- db.php
+++ db.php
@@ -2,9 +2,9 @@
 ob_start();
 session_start();
 define('DBHOST','sql2.freemysqlhosting.net');
-define('DBUSER','sql2218325');
-define('DBPASS','uR9*jA7*');
-define('DBNAME','sql2218325');
+define('DBUSER','sql12178521');
+define('DBPASS','DSXUgVzuyH');
+define('DBNAME','sql12178521');
 $db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 //date_default_timezone_set('Europe/London');
