b7c49b0ec48a346d4bc676617205e1f2bf41f5f5
Coder 79 <yahongie2014@users.noreply.github.com>
2018-02-15 17:48:59 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
 ob_start();
 session_start();
-define('DBHOST','sql2.freemysqlhosting.net');
-define('DBUSER','sql12178521');
-define('DBPASS','DSXUgVzuyH');
-define('DBNAME','sql12178521');
+define('DBHOST','sql302.epizy.com');
+define('DBUSER','epiz_21615192');
+define('DBPASS','Seri2020');
+define('DBNAME','epiz_21615192_db');
 $db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 //date_default_timezone_set('Europe/London');
