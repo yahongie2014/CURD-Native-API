96220a29ed7c6c4d10480bb62bcc47ab95795bd1
Coder 79 <dev.ahmed.saeed@gmail.com>
2018-08-08 05:53:46 +0200

    pgsql

--- db.php
+++ db.php
@@ -1,11 +1,11 @@
 <?php
 ob_start();
 session_start();
-define('DBHOST','sql302.epizy.com');
-define('DBUSER','epiz_21615192');
-define('DBPASS','Seri2020');
-define('DBNAME','epiz_21615192_db');
-$db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
+define('DBHOST','ec2-54-221-210-97.compute-1.amazonaws.com');
+define('DBUSER','oiptzecwhwkyrw');
+define('DBPASS','d72897b513637ac17aad064d9e5e4666bbbff209b100ee349ea934310ccc2ac4');
+define('DBNAME','d349qg1v184rmd');
+$db = new PDO("pgsql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 //date_default_timezone_set('Europe/London');
 ?>
